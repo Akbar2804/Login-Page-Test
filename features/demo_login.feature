229390b1281893b4f60261067766523da2b42a9f
Akbar Shaikh <akbaras090@gmail.com>
2025-04-22 22:55:26 +0530

    Demo file added

---
+++ features/demo_login.feature
@@ -0,0 +1,7 @@
+Feature: Validate the login page
+
+  Scenario: enter the login detail and click on submit
+  
+  Given Sample demo Website
+  When User enter username and password
+  Then User must go to home page
---
+++ features/login_demo.js
@@ -0,0 +1,15 @@
+         Given('Sample demo Website', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+
+         When('User enter username and password', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+         Then('User must go to home page', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
